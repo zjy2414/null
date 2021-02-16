259d9ccc895096ef8c9732dce33ea53f1b01957a
damyjy <zsqw123@vip.qq.com>
2021-02-16 09:10:42 +0800

    - change wx

--- app.js
+++ app.js
@@ -9,35 +9,40 @@ const download = require('download');
 
 // 公共变量
 const KEY = process.env.JD_COOKIE;
-const serverJ = process.env.PUSH_KEY;
+const WXBOT = process.env.PUSH_KEY;
 const DualKey = process.env.JD_COOKIE_2;
 
 
-async function downFile () {
-    // const url = 'https://cdn.jsdelivr.net/gh/NobyDa/Script@master/JD-DailyBonus/JD_DailyBonus.js'
-    const url = 'https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js';
-    await download(url, './');
+async function downFile() {
+  // const url = 'https://cdn.jsdelivr.net/gh/NobyDa/Script@master/JD-DailyBonus/JD_DailyBonus.js'
+  const url = 'https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js';
+  await download(url, './');
 }
 
-async function changeFile () {
-   let content = await fs.readFileSync('./JD_DailyBonus.js', 'utf8')
-   content = content.replace(/var Key = ''/, `var Key = '${KEY}'`);
-   if (DualKey) {
+async function changeFile() {
+  let content = await fs.readFileSync('./JD_DailyBonus.js', 'utf8')
+  content = content.replace(/var Key = ''/, `var Key = '${KEY}'`);
+  if (DualKey) {
     content = content.replace(/var DualKey = ''/, `var DualKey = '${DualKey}'`);
-   }
-   await fs.writeFileSync( './JD_DailyBonus.js', content, 'utf8')
+  }
+  await fs.writeFileSync('./JD_DailyBonus.js', content, 'utf8')
 }
 
-async function sendNotify (title,desp) {
-  const options ={
-    uri:  `https://sctapi.ftqq.com/${serverJ}.send`,
-    form: { title, desp },
+async function sendNotify(title, desp) {
+  const options = {
+    uri: WXBOT,
+    body: {
+      "msgtype": "text",
+      "text": {
+        "content": title + "\n" + desp
+      }
+    },
     json: true,
     method: 'POST'
   }
-  await rp.post(options).then(res=>{
+  await rp.post(options).then(res => {
     console.log(res)
-  }).catch((err)=>{
+  }).catch((err) => {
     console.log(err)
   })
 }
@@ -57,18 +62,18 @@ async function start() {
   await exec("node JD_DailyBonus.js >> result.txt");
   console.log('执行完毕')
 
-  if (serverJ) {
+  if (WXBOT) {
     const path = "./result.txt";
     let content = "";
     if (fs.existsSync(path)) {
       content = fs.readFileSync(path, "utf8");
     }
     let t = content.match(/【签到概览】:((.|\n)*)【签到总计】/)
-    let res = t ? t[1].replace(/\n/,'') : '失败'
+    let res = t ? t[1].replace(/\n/, '') : '失败'
     let t2 = content.match(/【签到总计】:((.|\n)*)【账号总计】/)
-    let res2 = t2 ? t2[1].replace(/\n/,'') : '总计0'
+    let res2 = t2 ? t2[1].replace(/\n/, '') : '总计0'
 
-    
+
     await sendNotify("" + ` ${res2} ` + ` ${res} ` + new Date().toLocaleDateString(), content);
   }
 }
